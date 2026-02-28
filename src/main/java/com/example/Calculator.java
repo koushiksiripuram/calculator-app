78de1fb4fb95ffe33879ac96dcd736c87176180e
koushiksiripuram <koushiksiripuram48@gmail.com>
2026-02-28 15:46:17 +0530

    Refactor Calculator class and add main method

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -1,9 +1,18 @@
 package com.example;
+
 public class Calculator {
-public int add(int a, int b) {
-return a + b;
-}
-public int subtract(int a, int b) {
-return a - b;
-}
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println("Add: " + c.add(10, 20));
+        System.out.println("Subtract: " + c.subtract(50, 30));
+    }
 }
